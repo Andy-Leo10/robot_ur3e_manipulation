173b676c1827f94b5411a58ec94ed48a201ee3aa
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-05-07 16:47:31 -0500

    Launch folder added

---
+++ launch/pick_and_place_primitive.launch.py
@@ -0,0 +1,36 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("my").to_moveit_configs()
+
+    # Declare the launch argument
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time',default_value='True',
+        description='Use simulation (Gazebo) clock if true')
+    # Use the launch argument in the node configuration
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # MoveItCpp demo executable
+    moveit_cpp_node = Node(
+        name="pick_and_place",
+        package="robot_ur3e_manipulation",
+        executable="pick_and_place_primitive",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {'use_sim_time': use_sim_time},
+            {'is_robot_sim': use_sim_time},
+        ],
+    )
+
+    return LaunchDescription(
+        [use_sim_time_arg,moveit_cpp_node]
+    )
