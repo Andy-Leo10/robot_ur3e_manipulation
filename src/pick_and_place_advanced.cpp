5124a889833c6eace81331b395ff6d3586365384
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-05-15 21:50:08 +0000

    Methods improved and new added. Is pending the main logic

--- src/pick_and_place_advanced.cpp
+++ src/pick_and_place_advanced.cpp
@@ -35,6 +35,8 @@ public:
         return node_->get_parameter("is_robot_sim").as_bool();
     }
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SCENE OBJECTs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     moveit_msgs::msg::CollisionObject createCollisionObject(
         const std::string& id, 
         const std::string& frame_id, 
@@ -71,7 +73,9 @@ public:
         pose.position.z = z;
         return pose;
     }
-
+    
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~JOINTs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
     void move_joint_space(float angle_0, float angle_1, float angle_2, float angle_3, float angle_4, float angle_5) {
         RCLCPP_INFO(LOGGER, "---------------------------MOVE JOINT STATE!----------------------------------");
         moveit::core::RobotStatePtr current_state = move_group_arm->getCurrentState(10);
@@ -97,6 +101,74 @@ public:
             RCLCPP_ERROR(LOGGER, "Joint space movement plan failed!");
         }
     }
+
+    void move_single_joint(const std::string& joint_name, double degree) {
+        RCLCPP_INFO(LOGGER, "---------------------------MOVE JOINT %s!----------------------------------", joint_name.c_str());
+        moveit::core::RobotStatePtr current_state = move_group_arm->getCurrentState(10);
+
+        std::vector<double> joint_group_positions;
+        current_state->copyJointGroupPositions(joint_model_group_arm, joint_group_positions);
+
+        double angle = degree * M_PI / 180.0;
+        if(joint_name == "shoulder_pan_joint") {
+            joint_group_positions[0] = angle;
+        } else if(joint_name == "shoulder_lift_joint") {
+            joint_group_positions[1] = angle;
+        } else if(joint_name == "elbow_joint") {
+            joint_group_positions[2] = angle;
+        } else if(joint_name == "wrist_1_joint") {
+            joint_group_positions[3] = angle;
+        } else if(joint_name == "wrist_2_joint") {
+            joint_group_positions[4] = angle;
+        } else if(joint_name == "wrist_3_joint") {
+            joint_group_positions[5] = angle;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Invalid joint name!");
+            return;
+        }
+
+        move_group_arm->setJointValueTarget(joint_group_positions);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+        bool success = (move_group_arm->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if(success) {
+            move_group_arm->execute(my_plan);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Joint space movement plan failed!");
+        }
+    }
+
+    void move_gripper_space(double distance) {
+        RCLCPP_INFO(LOGGER, "---------------------------MOVE GRIPPER JOINTS!----------------------------------");
+
+        std::vector<double> joint_group_positions;
+
+        move_group_gripper->getCurrentState()->copyJointGroupPositions(move_group_gripper->getCurrentState()->getRobotModel()->getJointModelGroup(move_group_gripper->getName()), joint_group_positions);
+
+        joint_group_positions[0] = distance; // rg2_gripper_finger_left_joint
+        joint_group_positions[1] = 0; // rg2_gripper_thumb_left_joint
+        joint_group_positions[2] = 0; // rg2_gripper_finger_right_joint
+        joint_group_positions[3] = 0; // rg2_gripper_thumb_right_joint
+
+        move_group_gripper->setJointValueTarget(joint_group_positions);
+
+        // Set planning time to 5 seconds
+        move_group_gripper->setPlanningTime(5.0);
+        // Set number of planning attempts to ..
+        move_group_gripper->setNumPlanningAttempts(10);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+        bool success = (move_group_gripper->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if(success) {
+            move_group_gripper->execute(my_plan);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Gripper space movement plan failed!");
+        }
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END EFFECTOR~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     void move_end_effector(double x, double y, double z, double roll_deg, double pitch_deg, double yaw_deg) {
         RCLCPP_INFO(LOGGER, "---------------------------MOVE END EFFECTOR!----------------------------------");
@@ -126,10 +198,74 @@ public:
             RCLCPP_ERROR(LOGGER, "End effector movement plan failed!");
         }
     }
+    
+    void position_end_effector(double x, double y, double z) {
+        RCLCPP_INFO(LOGGER, "---------------------------SET END EFFECTOR POSITION!----------------------------------");
+        geometry_msgs::msg::Pose target_pose;
+        // Get current end effector pose
+        geometry_msgs::msg::Pose current_pose = move_group_arm->getCurrentPose().pose;
+        target_pose.orientation.x = current_pose.orientation.x;
+        target_pose.orientation.y = current_pose.orientation.y;
+        target_pose.orientation.z = current_pose.orientation.z;
+        target_pose.orientation.w = current_pose.orientation.w;
+
+        // Set target position
+        target_pose.position.x = x;
+        target_pose.position.y = y;
+        target_pose.position.z = z;
+        move_group_arm->setPoseTarget(target_pose);
+
+        // Set planning time to 10 seconds
+        move_group_arm->setPlanningTime(10.0);
+        // Set number of planning attempts to ..
+        move_group_arm->setNumPlanningAttempts(100);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+        bool success = (move_group_arm->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if(success) {
+            move_group_arm->execute(my_plan);
+        } else {
+            RCLCPP_ERROR(LOGGER, "End effector movement plan failed!");
+        }
+    }
+
+    void orientation_end_effector(double roll_deg, double pitch_deg, double yaw_deg) {
+        RCLCPP_INFO(LOGGER, "---------------------------SET END EFFECTOR ORIENTATION!----------------------------------");
+        geometry_msgs::msg::Pose target_pose;
+        // Get current end effector pose
+        geometry_msgs::msg::Pose current_pose = move_group_arm->getCurrentPose().pose;
+        target_pose.position.x = current_pose.position.x;
+        target_pose.position.y = current_pose.position.y;
+        target_pose.position.z = current_pose.position.z;
+
+        // Set target orientation
+        tf2::Quaternion q;
+        q.setRPY(roll_deg * M_PI/180.0, pitch_deg * M_PI/180.0, yaw_deg * M_PI/180.0);
+        target_pose.orientation.x = q.x();
+        target_pose.orientation.y = q.y();
+        target_pose.orientation.z = q.z();
+        target_pose.orientation.w = q.w();
+        move_group_arm->setPoseTarget(target_pose);
+
+        // Set planning time to 10 seconds
+        move_group_arm->setPlanningTime(10.0);
+        // Set number of planning attempts to ..
+        move_group_arm->setNumPlanningAttempts(20);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+        bool success = (move_group_arm->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if(success) {
+            move_group_arm->execute(my_plan);
+        } else {
+            RCLCPP_ERROR(LOGGER, "End effector movement plan failed!");
+        }
+    }
 
     void move_waypoint(double delta, const std::string& direction) {
-        RCLCPP_INFO(LOGGER, "---------------------------MOVE WAYPOINT!----------------------------------");
-
+        RCLCPP_INFO(LOGGER, "---------------------------MOVE WAYPOINT %s!----------------------------------", direction.c_str());
+        
         std::vector<geometry_msgs::msg::Pose> waypoints;
         geometry_msgs::msg::Pose target_pose = move_group_arm->getCurrentPose().pose;
 
@@ -158,6 +294,8 @@ public:
             RCLCPP_ERROR(LOGGER, "Waypoint movement plan failed!");
         }
     }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~COMMANDs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     void cmd_arm(const std::string& target_name) {
         RCLCPP_INFO(LOGGER, "---------------------------ARM COMMAND!----------------------------------");
@@ -196,36 +334,11 @@ public:
         }
     }
 
-    void move_gripper_space(double distance) {
-        RCLCPP_INFO(LOGGER, "---------------------------MOVE GRIPPER JOINTS!----------------------------------");
-
-        std::vector<double> joint_group_positions;
-
-        move_group_gripper->getCurrentState()->copyJointGroupPositions(move_group_gripper->getCurrentState()->getRobotModel()->getJointModelGroup(move_group_gripper->getName()), joint_group_positions);
-
-        joint_group_positions[0] = distance; // rg2_gripper_finger_left_joint
-        joint_group_positions[1] = 0; // rg2_gripper_thumb_left_joint
-        joint_group_positions[2] = 0; // rg2_gripper_finger_right_joint
-        joint_group_positions[3] = 0; // rg2_gripper_thumb_right_joint
-
-        move_group_gripper->setJointValueTarget(joint_group_positions);
-
-        // Set planning time to 5 seconds
-        move_group_gripper->setPlanningTime(5.0);
-        // Set number of planning attempts to ..
-        move_group_gripper->setNumPlanningAttempts(10);
-
-        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-        bool success = (move_group_gripper->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
-        if(success) {
-            move_group_gripper->execute(my_plan);
-        } else {
-            RCLCPP_ERROR(LOGGER, "Gripper space movement plan failed!");
-        }
-    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HELPERs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     void print_end_effector_position() {
+        RCLCPP_INFO(LOGGER, "---------------------------END EFFECTOR POSE!----------------------------------");
+
         geometry_msgs::msg::PoseStamped current_pose = move_group_arm->getCurrentPose();
         RCLCPP_INFO(LOGGER, ">>>>>>>>>>>>>>>>>> Position: (%f, %f, %f)", current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z);
         RCLCPP_INFO(LOGGER, ">>>>>>>>>>>>>>>>>> Quaternion: (%f, %f, %f, %f)", current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w);
@@ -264,30 +377,65 @@ public:
         // angle_5 = Wrist 3
         return joint_values;
     }
-    
-    void move2xy(double x, double y) {
-        // Obtener la posición actual del efector final
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    void move2pos(double absolute_coord, const std::string& axis) {
+        RCLCPP_INFO(LOGGER, "---------------------------MOVE 2 POS %s!----------------------------------", axis.c_str());
+
+        std::vector<geometry_msgs::msg::Pose> waypoints;
+        // obtain the current position of the end effector
         geometry_msgs::msg::PoseStamped current_pose = move_group_arm->getCurrentPose();
+        geometry_msgs::msg::Pose target_pose = move_group_arm->getCurrentPose().pose;
 
-        // Calcular la diferencia en x y y
-        double dx = x - current_pose.pose.position.x;
-        double dy = y - current_pose.pose.position.y;
-
-        // Definir el tamaño del paso
-        double step_size = 0.01;
-
-        // Calcular el número de pasos
-        int num_steps_x = std::abs(dx / step_size);
-        int num_steps_y = std::abs(dy / step_size);
-
-        // Mover el efector final a la posición objetivo en pasos
-        for (int i = 0; i < num_steps_x; ++i) {
-            move_waypoint(dx > 0 ? step_size : -step_size, "x");
+        // calculate the difference depending on the axis
+        double difference = 0.0;
+        if(axis == "x") {
+            difference = absolute_coord - current_pose.pose.position.x;
+        } else if(axis == "y") {
+            difference = absolute_coord - current_pose.pose.position.y;
+        } else if(axis == "z") {
+            difference = absolute_coord - current_pose.pose.position.z;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Invalid axis!");
         }
-        for (int i = 0; i < num_steps_y; ++i) {
-            move_waypoint(dy > 0 ? step_size : -step_size, "y");
+
+        // set the step size
+        double step_size = 0.01;
+        // calculate the number of steps
+        int num_steps = std::abs(difference / step_size);
+
+        // move the end effector to the target position in steps
+        for (int i = 0; i < num_steps; ++i) {
+            if (axis == "x") {
+                target_pose.position.x += difference > 0 ? step_size : -step_size;
+            } 
+            else if (axis == "y") {
+                target_pose.position.y += difference > 0 ? step_size : -step_size;
+            } 
+            else if (axis == "z") {
+                target_pose.position.z += difference > 0 ? step_size : -step_size;
+            } 
+            else {
+                RCLCPP_ERROR(LOGGER, "Invalid axis: %s", axis.c_str());
+                return;
+            }
+            waypoints.push_back(target_pose);
+        }
+
+        moveit_msgs::msg::RobotTrajectory trajectory;
+        const double jump_threshold = 0.0;
+        const double eef_step = 0.01;
+
+        double fraction = move_group_arm->computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+
+        if(fraction >= 0.0) {
+            move_group_arm->execute(trajectory);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Waypoint movement plan failed!");
         }
     }
+
 private:
     rclcpp::Node::SharedPtr move_group_node;
     std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_arm;
@@ -342,25 +490,36 @@ int main(int argc, char **argv) {
     // Wait for MoveGroup to recieve and process the collision object message.
     rclcpp::sleep_for(std::chrono::seconds(2));
 
-    robotArm.cmd_arm("home");
-    float cube_pos_x_ = 0.26;
-    float cube_pos_y_ = 0.38;
+    robotArm.cmd_arm("crab");
+    float cube_pos_x_ = 0.310;
+    float cube_pos_y_ = 0.340;
     // read the atributte 'is_robot_sim' of the object
     if(robotArm.get_is_robot_sim()) {
         RCLCPP_INFO(LOGGER, "\n\n\n Robot is simulated \n\n\n");
-        robotArm.move_end_effector(cube_pos_x_, cube_pos_y_, 0.25, -180.0, 0.0, 0.0);
+        // robotArm.move_end_effector(cube_pos_x_, cube_pos_y_, 0.35, -180.0, 0.0, 0.0);
+        robotArm.print_end_effector_position();
+        // robotArm.orientation_end_effector(-180.0, 0.0, 0.0);
+        robotArm.move2pos(0.35, "z");
+        robotArm.move2pos(cube_pos_x_, "x");
+        robotArm.move2pos(cube_pos_y_, "y");
         robotArm.cmd_gripper("gripper_open");
         robotArm.print_end_effector_position();
         
-        robotArm.move_waypoint(-0.08, "z");
-        //robotArm.cmd_gripper("gripper_close");
-        robotArm.move_gripper_space(0.69);
+        robotArm.move_waypoint(-0.05, "z");
+        robotArm.cmd_gripper("gripper_close");
+        //robotArm.move_gripper_space(0.69);
+        robotArm.move2pos(0.20, "z");
+        // robotArm.move_waypoint(0.10, "z");
+        robotArm.move_single_joint("shoulder_pan_joint", 180.0);
 
-        robotArm.move_waypoint(0.10, "z");
-
-        robotArm.move_joint_space(6.1, -1.64, -1.59, -1.47, 1.56, -5.70);
+        // robotArm.move_end_effector(-0.5, -0.02, -0.15, -180.0, 0.0, 0.0);
+        robotArm.move2pos(-0.5, "x");
+        robotArm.move2pos(-0.02, "y");
+        robotArm.move2pos(-0.15, "z");
+        robotArm.print_end_effector_position();
         robotArm.cmd_gripper("gripper_open");
-    } else {
+    } 
+    else {
         RCLCPP_INFO(LOGGER, "\n\n\n Robot is real \n\n\n");
         robotArm.move_end_effector(cube_pos_x_, cube_pos_y_, 0.25, -180.0, 0.0, 0.0);
         robotArm.cmd_gripper("gripper_open");
